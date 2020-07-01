74a01fa8eb89a47369f9f72e8ae77fc0d5189bd9
vickythakur0398 <vickythakur0398@gmail.com>
2020-07-01 20:11:25 +0530

    here we have added one mmore controller and route file

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -1,4 +1,4 @@
 //same jo baki me kai tha ek function banaya h or use export krna h jise ki roy=te me use kar skae
-// module.exports.profile = function(req, res){
-//     res.end(`<h1>This is user profile</h1>`)
-// }
+module.exports.profile = function(req, res){
+    res.end(`<h1>This is user profile</h1>`)
+}
--- routes/user_router.js
+++ routes/user_router.js
@@ -1,14 +1,14 @@
 //all the same step ye humne user_controller ko access karne ke lie banaya h
-// const express = require('express');
+const express = require('express');
 
 
-// const router = express.Router;
+const router = express.Router;
 
-// const user_controller = require('../controllers/user_controllers' )
+const user_controller = require('../controllers/user_controllers' )
 
 
 
-// console.log('router loaded');
-// router.get('/profile', user_controller.profile );
+console.log('router loaded');
+router.get('/profile', user_controller.profile );
 
-// module.exports = router;
+module.exports = router;
