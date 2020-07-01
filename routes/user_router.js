7cb068f6f51998af8c2754ae9799df36be5dc1fb
vickythakur0398 <vickythakur0398@gmail.com>
2020-07-01 20:08:13 +0530

    here we have made controller and roouter file

---
+++ routes/user_router.js
@@ -0,0 +1,14 @@
+//all the same step ye humne user_controller ko access karne ke lie banaya h
+// const express = require('express');
+
+
+// const router = express.Router;
+
+// const user_controller = require('../controllers/user_controllers' )
+
+
+
+// console.log('router loaded');
+// router.get('/profile', user_controller.profile );
+
+// module.exports = router;
