c19b4125ab91850f4bbe76d6fd6fde2e3e9a2b11
vickythakur0398 <vickythakur0398@gmail.com>
2020-07-02 18:20:15 +0530

    created forms for signin and signup

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -9,3 +9,32 @@ return res.render('profile',{
     title: 'user profile'
 });
 }
+
+
+//we have to add action for signup and render the sign up page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title: "letterhost / sign Up"
+    })
+}
+
+
+//similar for the sign in
+//now after this we have view we have contoller actio and we need route ki is pe aie request aie to ye send kare route se browser ko
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title: "letterhost / sign IN"
+    })
+}
+
+//jo humne sign up form banaya h uska bhi router chaie to uska controller h ye  i.e to get the signup dta in database
+module.exports.create = function(req, res){
+    //will do it 
+    
+}
+
+// this is for singin to get that data
+module.exports.create = function(req, res){
+    //will do it
+
+}
--- routes/user_router.js
+++ routes/user_router.js
@@ -12,4 +12,8 @@ console.log('router loaded');
 // if we want to acces this router this router has expoerted itself so to acces this we can access via index,js main kyuki use ne to ise acquire kia h
 router.get('/profile', user_controller.profile );
 
+//here we are getting from the sgnup and sign in
+router.get('/sign-up', user_controller.signUp);
+router.get('/sign-in', user_controller.signIn);
+
 module.exports = router;
